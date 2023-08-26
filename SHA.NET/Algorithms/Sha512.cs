25f96ce7ff7c5ff6f44d4107f2bcf64a3a0892ae
Marcin Złotek <zlociu@onet.eu>
2023-08-26 11:43:58 +0200

    fix sha512

--- SHA.NET/Algorithms/Sha512.cs
+++ SHA.NET/Algorithms/Sha512.cs
@@ -73,26 +73,26 @@ public class Sha512
 
     private readonly static ulong[] kTable =
     {
-        0x428A2F98D728AE22, 0x7137449123EF65CD, 0xB5C0FBCFEC4D3B2F, 0xE9B5DBA58189DBBC,
-        0x3956C25BF348B538, 0x59F111F1B605D019, 0x923F82A4AF194F9B, 0xAB1C5ED5DA6D8118,
-        0xD807AA98A3030242, 0x12835B0145706FBE, 0x243185BE4EE4B28C, 0x550C7DC3D5FFB4E2,
-        0x72BE5D74F27B896F, 0x80DEB1FE3B1696B1, 0x9BDC06A725C71235, 0xC19BF174CF692694,
-        0xE49B69C19EF14AD2, 0xEFBE4786384F25E3, 0x0FC19DC68B8CD5B5, 0x240CA1CC77AC9C65,
-        0x2DE92C6F592B0275, 0x4A7484AA6EA6E483, 0x5CB0A9DCBD41FBD4, 0x76F988DA831153B5,
-        0x983E5152EE66DFAB, 0xA831C66D2DB43210, 0xB00327C898FB213F, 0xBF597FC7BEEF0EE4,
-        0xC6E00BF33DA88FC2, 0xD5A79147930AA725, 0x06CA6351E003826F, 0x142929670A0E6E70,
-        0x27B70A8546D22FFC, 0x2E1B21385C26C926, 0x4D2C6DFC5AC42AED, 0x53380D139D95B3DF,
-        0x650A73548BAF63DE, 0x766A0ABB3C77B2A8, 0x81C2C92E47EDAEE6, 0x92722C851482353B,
-        0xA2BFE8A14CF10364, 0xA81A664BBC423001, 0xC24B8B70D0F89791, 0xC76C51A30654BE30,
-        0xD192E819D6EF5218, 0xD69906245565A910, 0xF40E35855771202A, 0x106AA07032BBD1B8,
-        0x19A4C116B8D2D0C8, 0x1E376C085141AB53, 0x2748774CDF8EEB99, 0x34B0BCB5E19B48A8,
-        0x391C0CB3C5C95A63, 0x4ED8AA4AE3418ACB, 0x5B9CCA4F7763E373, 0x682E6FF3D6B2B8A3,
-        0x748F82EE5DEFB2FC, 0x78A5636F43172F60, 0x84C87814A1F0AB72, 0x8CC702081A6439EC,
-        0x90BEFFFA23631E28, 0xA4506CEBDE82BDE9, 0xBEF9A3F7B2C67915, 0xC67178F2E372532B,
-        0xCA273ECEEA26619C, 0xD186B8C721C0C207, 0xEADA7DD6CDE0EB1E, 0xF57D4F7FEE6ED178,
-        0x06F067AA72176FBA, 0x0A637DC5A2C898A6, 0x113F9804BEF90DAE, 0x1B710B35131C471B,
-        0x28DB77F523047D84, 0x32CAAB7B40C72493, 0x3C9EBE0A15C9BEBC, 0x431D67C49C100D4C,
-        0x4CC5D4BECB3E42B6, 0x597F299CFC657E2A, 0x5FCB6FAB3AD6FAEC, 0x6C44198C4A475817
+        0x428A2F98D728AE22U, 0x7137449123EF65CDU, 0xB5C0FBCFEC4D3B2FU, 0xE9B5DBA58189DBBCU,
+        0x3956C25BF348B538U, 0x59F111F1B605D019U, 0x923F82A4AF194F9BU, 0xAB1C5ED5DA6D8118U,
+        0xD807AA98A3030242U, 0x12835B0145706FBEU, 0x243185BE4EE4B28CU, 0x550C7DC3D5FFB4E2U,
+        0x72BE5D74F27B896FU, 0x80DEB1FE3B1696B1U, 0x9BDC06A725C71235U, 0xC19BF174CF692694U,
+        0xE49B69C19EF14AD2U, 0xEFBE4786384F25E3U, 0x0FC19DC68B8CD5B5U, 0x240CA1CC77AC9C65U,
+        0x2DE92C6F592B0275U, 0x4A7484AA6EA6E483U, 0x5CB0A9DCBD41FBD4U, 0x76F988DA831153B5U,
+        0x983E5152EE66DFABU, 0xA831C66D2DB43210U, 0xB00327C898FB213FU, 0xBF597FC7BEEF0EE4U,
+        0xC6E00BF33DA88FC2U, 0xD5A79147930AA725U, 0x06CA6351E003826FU, 0x142929670A0E6E70U,
+        0x27B70A8546D22FFCU, 0x2E1B21385C26C926U, 0x4D2C6DFC5AC42AEDU, 0x53380D139D95B3DFU,
+        0x650A73548BAF63DEU, 0x766A0ABB3C77B2A8U, 0x81C2C92E47EDAEE6U, 0x92722C851482353BU,
+        0xA2BFE8A14CF10364U, 0xA81A664BBC423001U, 0xC24B8B70D0F89791U, 0xC76C51A30654BE30U,
+        0xD192E819D6EF5218U, 0xD69906245565A910U, 0xF40E35855771202AU, 0x106AA07032BBD1B8U,
+        0x19A4C116B8D2D0C8U, 0x1E376C085141AB53U, 0x2748774CDF8EEB99U, 0x34B0BCB5E19B48A8U,
+        0x391C0CB3C5C95A63U, 0x4ED8AA4AE3418ACBU, 0x5B9CCA4F7763E373U, 0x682E6FF3D6B2B8A3U,
+        0x748F82EE5DEFB2FCU, 0x78A5636F43172F60U, 0x84C87814A1F0AB72U, 0x8CC702081A6439ECU,
+        0x90BEFFFA23631E28U, 0xA4506CEBDE82BDE9U, 0xBEF9A3F7B2C67915U, 0xC67178F2E372532BU,
+        0xCA273ECEEA26619CU, 0xD186B8C721C0C207U, 0xEADA7DD6CDE0EB1EU, 0xF57D4F7FEE6ED178U,
+        0x06F067AA72176FBAU, 0x0A637DC5A2C898A6U, 0x113F9804BEF90DAEU, 0x1B710B35131C471BU,
+        0x28DB77F523047D84U, 0x32CAAB7B40C72493U, 0x3C9EBE0A15C9BEBCU, 0x431D67C49C100D4CU,
+        0x4CC5D4BECB3E42B6U, 0x597F299CFC657E2AU, 0x5FCB6FAB3AD6FAECU, 0x6C44198C4A475817U
     };
 
     private ulong[] result = Array.Empty<ulong>();
@@ -145,17 +145,18 @@ public class Sha512
         ulong[] buffer = ArrayPool<ulong>.Shared.Rent(80);
         byte* offsetPtr = data;
         int k = 0;
+
         while (k < 16)
         {
             buffer[k] =
-                (((uint)offsetPtr[(k * 8) + 0]) << 56) +
-                (((uint)offsetPtr[(k * 8) + 1]) << 48) +
-                (((uint)offsetPtr[(k * 8) + 2]) << 40) +
-                (((uint)offsetPtr[(k * 8) + 3]) << 32) +
-                (((uint)offsetPtr[(k * 8) + 4]) << 24) +
-                (((uint)offsetPtr[(k * 8) + 5]) << 16) +
-                (((uint)offsetPtr[(k * 8) + 6]) << 8) +
-                (((uint)offsetPtr[(k * 8) + 7]) << 0);
+                (((ulong)offsetPtr[(k * 8) + 0]) << 56) +
+                (((ulong)offsetPtr[(k * 8) + 1]) << 48) +
+                (((ulong)offsetPtr[(k * 8) + 2]) << 40) +
+                (((ulong)offsetPtr[(k * 8) + 3]) << 32) +
+                (((ulong)offsetPtr[(k * 8) + 4]) << 24) +
+                (((ulong)offsetPtr[(k * 8) + 5]) << 16) +
+                (((ulong)offsetPtr[(k * 8) + 6]) << 8) +
+                (((ulong)offsetPtr[(k * 8) + 7]) << 0);
 
             k++;
         }
@@ -164,6 +165,11 @@ public class Sha512
         {
             buffer[k] = SmallSigma1(buffer[k - 2]) + buffer[k - 7] + SmallSigma0(buffer[k - 15]) + buffer[k - 16];
             k++;
+        }
+
+        foreach (var b in buffer)
+        {
+            Console.Write("{0} ", b);
         }
 
         ulong tmp1, tmp2 = 0;
@@ -218,31 +224,35 @@ public class Sha512
 
         padding[k + data_len_mod_0x7F - 1] = 0x80;
 
-        UInt128 wholeSize = (UInt128)length;
-        wholeSize <<= 3;
-        byte[] wholeSizePtr = new byte[16];
-        fixed (byte* numPtr = &wholeSizePtr[0])
+        ulong wholeSizeSmall = (ulong)(length << 3);
+        ulong wholeSize  = (ulong)((length >> 61) & 0x07);
+        byte* wholeSizePtrSmall = (byte*)&wholeSizeSmall;
+        byte* wholeSizePtr = (byte*)&wholeSize;
+
+        padding[255] = wholeSizePtrSmall[0];
+        padding[254] = wholeSizePtrSmall[1];
+        padding[253] = wholeSizePtrSmall[2];
+        padding[252] = wholeSizePtrSmall[3];
+        padding[251] = wholeSizePtrSmall[4];
+        padding[250] = wholeSizePtrSmall[5];
+        padding[249] = wholeSizePtrSmall[6];
+        padding[248] = wholeSizePtrSmall[7];
+
+        padding[247] = wholeSizePtr[0];
+        padding[246] = wholeSizePtr[1];
+        padding[245] = wholeSizePtr[2];
+        padding[244] = wholeSizePtr[3];
+        padding[243] = wholeSizePtr[4];
+        padding[242] = wholeSizePtr[5];
+        padding[241] = wholeSizePtr[6];
+        padding[240] = wholeSizePtr[7];
+
+        foreach (byte b in padding.AsSpan().Slice(128))
         {
-            *(UInt128*) numPtr = wholeSize;
+            Console.Write("{0} ", b);
         }
 
-        padding[255] = wholeSizePtr[0];
-        padding[254] = wholeSizePtr[1];
-        padding[253] = wholeSizePtr[2];
-        padding[252] = wholeSizePtr[3];
-        padding[251] = wholeSizePtr[4];
-        padding[250] = wholeSizePtr[5];
-        padding[249] = wholeSizePtr[6];
-        padding[248] = wholeSizePtr[7];
-
-        padding[247] = wholeSizePtr[8];
-        padding[246] = wholeSizePtr[9];
-        padding[245] = wholeSizePtr[10];
-        padding[244] = wholeSizePtr[11];
-        padding[243] = wholeSizePtr[12];
-        padding[242] = wholeSizePtr[13];
-        padding[241] = wholeSizePtr[14];
-        padding[240] = wholeSizePtr[15];
+        Console.WriteLine(data_len_mod_0x7F);
 
         fixed (byte* ptr = &padding[0])
         {
--- SHA.NET/Program.cs
+++ SHA.NET/Program.cs
@@ -2,11 +2,11 @@
 using System.Text;
 using SHA.Algorithms;
 
-var sha = new Sha256();
+var sha = new Sha512();
 var bytes = File.ReadAllBytes("przykladowy_ndl.txt");
 var s1 = new System.Diagnostics.Stopwatch();
 s1.Start();
-sha.ComputeHash(bytes);
+sha.ComputeHash("ABCDEFGHIJKLMNOPQRSTUVWXYZ"u8.ToArray());
 s1.Stop();
 Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
 Console.WriteLine(sha.GetHash());
