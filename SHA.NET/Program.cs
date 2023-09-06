0811202196ac848d61b3d29215f3279857541158
Marcin Złotek <zlociu@onet.eu>
2023-09-06 19:52:51 +0200

    connect projects

--- SHA.NET/Program.cs
+++ SHA.NET/Program.cs
@@ -1,19 +1,18 @@
-﻿
-using System.Text;
-using SHA.Algorithms;
+﻿// using System.Text;
+// using SHA.Algorithms;
 
-var sha = new Sha256();
-var bytes = File.ReadAllBytes("przykladowy_ndl.txt");
-var s1 = new System.Diagnostics.Stopwatch();
-s1.Start();
-sha.ComputeHash(bytes);
-s1.Stop();
-Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
-Console.WriteLine(sha.Hash);
+// var sha = new Sha256();
+// var bytes = File.ReadAllBytes("przykladowy_ndl.txt");
+// var s1 = new System.Diagnostics.Stopwatch();
+// s1.Start();
+// sha.ComputeHash(bytes);
+// s1.Stop();
+// Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
+// Console.WriteLine(sha.Hash);
 
-using var stream = File.OpenRead("przykladowy_ndl.txt");
-s1.Restart();
-sha.ComputeHash(stream);
-s1.Stop();
-Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
-Console.WriteLine(sha.Hash);
+// using var stream = File.OpenRead("przykladowy_ndl.txt");
+// s1.Restart();
+// sha.ComputeHash(stream);
+// s1.Stop();
+// Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
+// Console.WriteLine(sha.Hash);
--- SHA_Checksum/Program.cs
+++ SHA_Checksum/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SHA.Algorithms;
+
+internal class Program
+{
+    private static void ParseCmdArguments(string[] args)
+    {
+        
+    }
+
+    private static void Main(string[] args)
+    {
+        var sha = new Sha256();
+        var bytes = File.ReadAllBytes("przykladowy_ndl.txt");
+        var s1 = new System.Diagnostics.Stopwatch();
+        s1.Start();
+        sha.ComputeHash(bytes);
+        s1.Stop();
+        Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
+        Console.WriteLine(sha.Hash);
+
+        using var stream = File.OpenRead("przykladowy_ndl.txt");
+        s1.Restart();
+        sha.ComputeHash(stream);
+        s1.Stop();
+        Console.WriteLine("{0} ms", s1.ElapsedMilliseconds);
+        Console.WriteLine(sha.Hash);
+    }
+}
