66ab85d684ccbeb9be903188c4359da804a10f73
Marcin Złotek <zlociu@onet.eu>
2023-09-16 18:47:29 +0200

    add registry config file

---
+++ SHA_Checksum/config_register.fsx
@@ -0,0 +1,29 @@
+//add to registry to context menu for all files options to calculate SHA checksum for all SHA algorithms in F#
+
+open System
+open Microsoft.Win32
+
+let mainKey = Registry.ClassesRoot.CreateSubKey("*\\shell\\SHA")
+mainKey.SetValue("ExtendedSubCommandsKey", "*\\shell\\SHA")
+mainKey.SetValue("MUIVerb", "SHA Checksum")
+mainKey.SetValue("Icon", __SOURCE_DIRECTORY__ + "\\icon.ico")
+
+let subKey = mainKey.CreateSubKey("Shell")
+let subKey1 = subKey.CreateSubKey("SHA-1")
+let subKey224 = subKey.CreateSubKey("SHA-224")
+let subKey256 = subKey.CreateSubKey("SHA-256")
+let subKey384 = subKey.CreateSubKey("SHA-384")
+let subKey512 = subKey.CreateSubKey("SHA-512")
+
+let exePath = __SOURCE_DIRECTORY__ + "\\bin\\Release\\net7.0\\SHA_Checksum.exe"
+
+subKey1.CreateSubKey("command").SetValue("", exePath + " SHA1 \"%1\"")
+subKey1.SetValue("CommandFlags", 0x40, RegistryValueKind.DWord)
+
+subKey224.CreateSubKey("command").SetValue("", exePath + " SHA224 \"%1\"")
+subKey256.CreateSubKey("command").SetValue("", exePath + " SHA256 \"%1\"")
+subKey384.CreateSubKey("command").SetValue("", exePath + " SHA384 \"%1\"")
+subKey512.CreateSubKey("command").SetValue("", exePath + " SHA512 \"%1\"")
+
+
+
